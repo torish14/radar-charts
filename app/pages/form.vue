329f27610c1f2db62dacfea19ea508d61c3c9aab
toshihirokato <49876561+toshihirokato@users.noreply.github.com>
2020-01-19 09:12:01 +0900

    delete excrescence

--- app/pages/form.vue
+++ app/pages/form.vue
@@ -1,47 +1,41 @@
 <template>
   <section class="container">
-    <div class="upload">
-      <Upload />
-      <div class="radar">
-        <!-- ラベルの色の濃さを上位から順に薄くしていく -->
-        <radar-chart
-          :chart-data="datacollection"
-          :options="chartOptions"
-          :bind="true"
-        />
-        <form @submit.prevent="addData">
-          <input v-model="dataentry" placeholder="Add a Data">
-          <input v-model="datalabel" placeholder="Add a Label">
-          <button type="submit">
-            Submit
-          </button>
-        </form>
-      </div>
+    <div class="radar">
+      <radar-chart
+        :chart-data="datacollection"
+        :options="chartOptions"
+        :bind="true"
+      />
+      <form @submit.prevent="addData">
+        <input v-model="dataentry" placeholder="Add a Data">
+        <input v-model="datalabel" placeholder="Add a Label">
+        <button type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   </section>
 </template>
 
 <script>
 import RadarChart from './RadarChart.js'
-import Upload from '~/components/Upload.vue'
 
 export default {
   components: {
-    Upload,
     RadarChart
   },
   data () {
     return {
       datacollection: {
-        labels: ['1: 自己確信', '2: 指令性', '3: 競争性', '4: 着想', '5: 戦略性'],
+        labels: ['a', 'b', 'c', 'd', 'e'],
         datasets: [
           {
-            backgroundColor: 'rgba(255, 204, 0, 0.4)', // 塗り潰しの色
-            borderColor: 'rgba(255, 204, 0, 1)', // 線の色
-            pointBackgroundColor: 'rgba(255, 204, 0, 1)', // 点の塗り潰しの色
-            pointBorderColor: '#fff', // 点の境界線の色
-            pointHoverBackgroundColor: '#fff', // マウスオーバー時の点の背景色
-            pointHoverBorderColor: 'rgba(255, 204, 0, 1)', // マウスオーバー時の点の境界線の色
+            backgroundColor: 'rgba(255, 204, 0, 0.4)',
+            borderColor: 'rgba(255, 204, 0, 1)',
+            pointBackgroundColor: 'rgba(255, 204, 0, 1)', 
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff', 
+            pointHoverBorderColor: 'rgba(255, 204, 0, 1)', 
             data: [100, 90, 80, 70, 60], // numbers
             fill: true,
             pointHitRadius: 20,
@@ -84,13 +78,6 @@ export default {
 </script>
 
 <style>
-.upload {
-  height: 300px;
-  width: 300px;
-  margin: 20px 20px;
-  padding: 20px 20px;
-}
-
 .radar {
   height: 300px;
   width: 300px;
